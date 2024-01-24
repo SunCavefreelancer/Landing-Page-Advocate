f1393b86ccb33bfa9a2d1027eaa93fed23f0ead0
Code_GG <139456030+SunCavefreelancer@users.noreply.github.com>
2024-01-24 04:35:17 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,36 +2,7 @@
 "use strict"
 
 
-const icon = document.querySelector('.icon-menu');
-icon.addEventListener('click', function () {
-	document.documentElement.classList.toggle('menu-open');
-});
-
-/* ---------------------------------БУРГЕР МЕНЮ---------------------------------------- */
-
-/* =====================================sliderFON=============================================== */
-
-
-$('.slider').slick({
-    arrows:true,   /* кнопки: true - включить, false - выключить */   
-    dots: true,      /* точки для переключения кратинок */  
-    infinite: true,  /* бесконечное перелистывание фото */
-    fade:true,     /* будет только 1 фото видна, что нам и нужно */
-    speed: 500,    /* со скоростью 1 сек. */
-    slidesToShow: 1, /* здесь указываем количество слайдов, которое хотим отобразить за 1 раз */   
-    slidesToScroll:1, /* количество слайдом за 1 пролистывание */
-    adaptiveHeight: true, /* автоматическая Адаптивная высота слайда-картинки */
-
-    autoplay:true, /* авто перелистывание */
-    swipe:true, /* на мобильных устройствах можно свайпать фото пальцем */
-    cssEase: 'linear' /* классный эффект перелистывания */
-  });
-
-/* =====================================sliderFON=============================================== */
-
-
-
-/*---class = .main__upbutton-----------кнопка-------------javascript---------------------*/
+/*---class = .advantages__item-----------кнопка-------------javascript---------------------*/
 let options = {
 	root: null,
 	rootMargin: "0px 0px 0px 0px",
@@ -46,7 +17,7 @@ let callback = (entries, observer) => {
 			targetElement.classList.add("show");
 			console.log('я тебя вижу');
 		} else {
-			//targetElement/classList.remove('show');
+			targetElement/classList.remove("show");
 			console.log('я тебя не вижу');
 		}
 	});
@@ -54,6 +25,10 @@ let callback = (entries, observer) => {
 
 let observer = new IntersectionObserver(callback, options);
 
-//если 1 обьект
-let target = document.querySelector(".main__upbutton");
-observer.observe(target);
+
+//если обьектов много (с модификатором --anim)
+let someElements = document.querySelectorAll("[class*='--anim']");
+someElements.forEach(someElement => {
+	observer.observe(someElement);
+});
+/*---class = .advantages__item-----------кнопка-------------javascript---------------------*/
